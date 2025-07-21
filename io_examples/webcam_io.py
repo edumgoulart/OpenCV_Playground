341ad948f02cff325e52bb897e438b5b2764d448
edumgoulart <edugoulart22@gmail.com>
2025-07-21 16:59:21 -0300

    webcam.io

---
+++ io_examples/webcam_io.py
@@ -0,0 +1,16 @@
+import cv2
+
+#reading webcam
+webcam = cv2.VideoCapture(0)
+
+#visualizing webcam
+
+while True:
+    ret, frame = webcam.read()
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(40) & 0xFF == ord('q'):
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
